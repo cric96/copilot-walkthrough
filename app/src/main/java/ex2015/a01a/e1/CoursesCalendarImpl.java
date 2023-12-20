92401e5a9edcfb5efe223eb4ea17c9f1c4c8594b
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-12-20 11:17:29 +0100

    :rocket: start as baseline

---
+++ app/src/main/java/ex2015/a01a/e1/CoursesCalendarImpl.java
@@ -0,0 +1,29 @@
+package ex2015.a01a.e1;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class CoursesCalendarImpl implements CoursesCalendar {
+
+    @Override
+    public List<Integer> possibleSlots() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void bookRoom(Day day, Room room, int start, int duration, String course) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Set<Pair<Integer, String>> dayRoomSlots(Day d, Room r) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Map<Pair<Day, Room>, Set<Integer>> courseSlots(String course) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
